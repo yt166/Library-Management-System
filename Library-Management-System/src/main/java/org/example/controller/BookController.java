343dc6315a93079bcab09cce2568a41c134b247c
Yuwani Ranasinghe <yuwaniranasinghe@gmail.com>
2024-06-13 06:07:40 +0530

    hibernate the project and connect the project with the data base

--- Library-Management-System/src/main/java/org/example/controller/BookController.java
+++ Library-Management-System/src/main/java/org/example/controller/BookController.java
@@ -4,6 +4,7 @@ import org.example.dto.Book;
 import org.example.service.BookServive;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +14,7 @@ public class BookController {
     @Autowired
     BookServive bookServive;
     @PostMapping
-    public void addBook(Book book){
+    public void addBook(@RequestBody Book book){
         bookServive.addBook(book);
 
     }
